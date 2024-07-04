ccb092529c5e00b11935333be3fb46851df4e330
ikramWebDev <m.ikramlatif09@gmail.com>
2024-07-04 21:21:39 +0500

    sdfkihgbfs

---
+++ data.js
@@ -0,0 +1,17 @@
+const aboutCard = [
+    {
+      img: "./assets/check icon.png",
+      h4: "Creative design",
+      paragraph: "Through a blend of aesthetics and functionality, CollegeOnly flaunts a sleek and new design with every element attentively crafted to improve the college experience and user engagement.",
+    },
+    {
+      img: "./assets/check icon.png",
+      h4: "Easy to use",
+      paragraph: "CollegeOnly's intuitive interface makes it easier to manage. Whether you are sending friend requests or checking any event updates, everything is just a click away.",
+    },
+    {
+      img: "./assets/check icon.png",
+      h4: "Best user experience",
+      paragraph: "Relish all-in-one interactions with the CollegeOnly app that optimize performance, ensuring fast load times, minimal glitches, and an overall smooth user experience.",
+    },
+  ];
